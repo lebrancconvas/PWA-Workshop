9d4677993ff01cbd572a77da24d9d5ce8d27d223
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-14 20:57:18 +0700

    Create Service Worker file on Public Folder.

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open('video-store').then(function(cache){
+      return cache.addAll([
+        'index.html',
+        'assets/css/style.css',
+        'assets/js/index.js',
+        'assets/images/cherprang.jpg',
+        'assets/images/music.jpg',
+        'assets/images/satchan.jpg',
+        'assets/images/sport.jpg',
+        'assets/images/tarwann02.gif'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response){
+      return response || fetch(e.request);
+    })
+  );
+});
